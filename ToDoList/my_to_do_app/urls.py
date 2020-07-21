35c94e3c4f8d151856523b7f1cc9b3cc80c6bea3
smallunix <smallunix@naver.com>
2020-07-21 21:59:01 +0900

    2020-07-21 21:58

---
+++ ToDoList/my_to_do_app/urls.py
@@ -0,0 +1,10 @@
+#my_to_do_app > urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('createTodo/', views.createTodo, name='createTodo'),
+    path('deleteTodo/', views.doneTodo, name='deleteTodo')
+]
---
+++ ToDoList/my_to_do_app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
+
+# Create your views here.
+def index(request):
+    #return HttpResponse("my_to_do_app first page.. 한글도 되나")
+    todos = Todo.objects.all()
+    content = {'todos':todos}
+    return render(request, 'my_to_do_app/index.html', content)
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content=user_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+    #return HttpResponse("Create Todo를 할거야! =>" + user_input_str) 
+
+def doneTodo(request):
+    done_todo_id = request.GET['todoNum']
+    print("=================== 완료한 todo의 id : ", done_todo_id)
+    todo = Todo.objects.get(id = done_todo_id)
+    todo.delete()
+    return HttpResponseRedirect(reverse('index'))
